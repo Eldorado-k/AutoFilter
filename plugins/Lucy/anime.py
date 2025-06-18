f2db1c77a6f046e4ac5c8611a2e3433c19c0ac69
Eldorado-k <abdoulmadjid22k@gmail.com>
2025-06-18 13:55:06 +0000

    Update anime.py

--- plugins/Lucy/anime.py
+++ plugins/Lucy/anime.py
@@ -145,16 +145,16 @@ async def get_anime(title):
             "https://graphql.anilist.co",
             json={"query": anime_query, "variables": title},
         )
-        return await r.json()
+        return await r.read()
 
 
 def shorten(description, info="anilist.co"):
     ms_g = ""
     if len(description) > 700:
         description = f"{description[:500]}...."
-        ms_g += f'\n<strong>Description :</strong> <em>{description}</em><a href="{info}">Plus d\'infos</a>'
+        ms_g += f'\n<strong>Description:</strong> <em>{description}</em><a href="{info}">More info</a>'
     else:
-        ms_g += f"\n<strong>Description :</strong> <em>{description}</em>"
+        ms_g += f"\n<strong>Description:</strong> <em>{description}</em>"
     return (
         ms_g.replace("<br>", "")
         .replace("</br>", "")
@@ -166,48 +166,39 @@ def shorten(description, info="anilist.co"):
 @Client.on_message(filters.command("anime", '/'))
 async def anime_search(_, mesg):
     search = mesg.text.split(None, 1)
-    reply = await mesg.reply("⏳ <i>Veuillez patienter...</i>", quote=True)
+    reply = await mesg.reply("⏳ <i>Please wait ...</i>", quote=True)
     if len(search) == 1:
-        return await reply.edit("⚠️ <b>Veuillez indiquer un nom d'anime.</b>")
+        return await reply.edit("⚠️ <b>Give Anime name please.</b>")
     else:
         search = search[1]
     variables = {"search": search}
-    try:
-        res = (await get_anime(variables))["data"].get("Media", None)
-        if not res:
-            return await reply.edit("💢 Aucun anime trouvé ! [404]")
-    except Exception as e:
-        return await reply.edit(f"❌ Erreur API : {str(e)}")
-
+    if not (res := json.loads(await get_anime(variables))["data"].get("Media", None)):
+        return await reply.edit("💢 No Resource Anime found! [404]")
     durasi = (
         get_readable_time(int(res.get("duration") * 60))
         if res.get("duration") is not None
         else "0"
     )
-    msg = f"<b>{res['title']['romaji']}</b> (<code>{res['title']['native']}</code>)\n<b>Type</b>: {res['format']}\n<b>Statut</b>: {res['status']}\n<b>Épisodes</b>: {res.get('episodes', 'N/A')}\n<b>Durée</b>: {durasi} par épisode.\n<b>Score</b>: {res['averageScore']}%\n<b>Catégorie</b>: <code>"
+    msg = f"<b>{res['title']['romaji']}</b> (<code>{res['title']['native']}</code>)\n<b>Type</b>: {res['format']}\n<b>Status</b>: {res['status']}\n<b>Episodes</b>: {res.get('episodes', 'N/A')}\n<b>Duration </b>: {durasi} Per Eps.\n<b>Score</b>: {res['averageScore']}%\n<b>Category</b>: <code>"
     for x in res["genres"]:
         msg += f"{x}, "
     msg = msg[:-2] + "</code>\n"
-    
     try:
         sd = res["startDate"]
         startdate = str(f"{month_name[sd['month']]} {sd['day']}, {sd['year']}")
     except:
         startdate = "-"
-    msg += f"<b>Date de début</b>: <code>{startdate}</code>\n"
-    
+    msg += f"<b>Start date</b>: <code>{startdate}</code>\n"
     try:
         ed = res["endDate"]
         enddate = str(f"{month_name[ed['month']]} {ed['day']}, {ed['year']}")
     except:
         enddate = "-"
-    msg += f"<b>Date de fin</b>: <code>{enddate}</code>\n"
-    
+    msg += f"<b>End date</b>: <code>{enddate}</code>\n"
     msg += "<b>Studios</b>: <code>"
     for x in res["studios"]["nodes"]:
         msg += f"{x['name']}, "
     msg = msg[:-2] + "</code>\n"
-    
     info = res.get("siteUrl")
     trailer = res.get("trailer", None)
     if trailer:
@@ -215,38 +206,35 @@ async def anime_search(_, mesg):
         site = trailer.get("site", None)
         if site == "youtube":
             trailer = f"https://youtu.be/{trailer_id}"
-    
     description = (
-        res.get("description", "N/A")
+        res.get("description")
         .replace("<i>", "")
         .replace("</i>", "")
         .replace("<br>", "")
+        if res.get("description") is not None
+        else "N/A"
     )
     msg += shorten(description, info)
-    image = info.replace("anilist.co/anime/", "img.anili.st/media/") if info else None
-    
+    image = info.replace("anilist.co/anime/", "img.anili.st/media/")
     btn = (
         [
             [
-                InlineKeyboardButton("Plus d'infos", url=info),
-                InlineKeyboardButton("Bande-annonce 🎬", url=trailer),
+                InlineKeyboardButton("More info", url=info),
+                InlineKeyboardButton("Bande Annonce 🎬", url=trailer),
             ]
         ]
         if trailer
-        else [[InlineKeyboardButton("Plus d'infos", url=info)]]
+        else [[InlineKeyboardButton("More info", url=info)]]
     )
 
     if image:
         try:
             await mesg.reply_photo(
-                image, 
-                caption=msg, 
-                reply_markup=InlineKeyboardMarkup(btn)
-        except Exception as e:
-            print(f"Erreur d'envoi photo: {e}")
+                image, caption=msg, reply_markup=InlineKeyboardMarkup(btn)
+            )
+        except:
             msg += f" [〽️]({image})"
-            await reply.edit(msg, reply_markup=InlineKeyboardMarkup(btn) if btn else None)
+            await reply.edit(msg)
     else:
-        await reply.edit(msg, reply_markup=InlineKeyboardMarkup(btn) if btn else None)
-    
+        await reply.edit(msg)
     await reply.delete()
